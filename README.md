a7805c08aa254e63ce09f12cf18e8797e781a94f
Vedant-Asati03 <vedant.asati03@gmail.com>
2022-10-28 20:37:27 +0530

    image in folder + credentials via json

--- README.md
+++ README.md
@@ -1,29 +1,20 @@
-# SPAMZEE
+# SpamZee
+
+![Spamzee](./assets/Spamzee.jpg)
 
 Hello guys! This is a fun little project called spamzee, you can use it to spam your friends, family or whoever you want.
 
 
 
-# ABOUT
+# About
 
 Spamzee is an automated bot which lets you spam anywhere you want, though it is specially designed for spamming in `Whatsapp`, `Telegram` and `Email` you can spam on other platforms too by **typing input** `others`.
 
 
+<br>
 
 
-_______________________________________________________________________________________________________________________________________________________________________
-
-
-![Spamzee](https://user-images.githubusercontent.com/109758134/197852239-a4fcd37e-bfbf-45c2-b6bb-f58363708578.jpg)
-
-
-_______________________________________________________________________________________________________________________________________________________________________
-
-
-
-
-# INSTALLATION
-
+# Installation
 
 For running this project you need to **install** following modules
 
--- spammer.py
+++ spammer.py
@@ -1,4 +1,5 @@
 from termcolor import colored
+import json
 from AppOpener import run
 import smtplib
 import ssl
@@ -6,78 +7,88 @@ import pyautogui
 import time
 
 
+def get_credentials() -> dict[str, str]:
+    fpath = "./credentials.json"
+    with open(fpath) as f:
+        content = json.load(f)
+        return content
+
+
 def open_app(x):
-    '''Opens app'''
+    """Opens app"""
     run(x)
 
 
 def search(y):
-    '''Searches from contacts'''
+    """Searches from contacts"""
     time.sleep(1)
     pyautogui.write(y)
-    pyautogui.press('Enter')
+    pyautogui.press("Enter")
 
 
 def spamit(z):
-    '''Spams in the chat'''
+    """Spams in the chat"""
     pyautogui.write(z)
-    pyautogui.press('Enter')
+    pyautogui.press("Enter")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(
         colored(
-            '\n\n===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===\n',
-            'cyan',
+            "\n\n===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===\n",
+            "cyan",
         )
     )
     print(
         colored(
-            '                                                                            SPAMZEE                             ',
-            'cyan',
-            attrs=['bold'],
+            "                                                                            SPAMZEE                             ",
+            "cyan",
+            attrs=["bold"],
         )
     )
     print(
         colored(
-            '\n===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===\n\n',
-            'cyan',
+            "\n===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===*===\n\n",
+            "cyan",
         )
     )
 
-    spam = input(colored('|What you want to spam| : ', 'yellow')).capitalize()
+    spam = input(colored("|What you want to spam| : ", "yellow")).capitalize()
 
     whom = input(
-        colored('|Who do you want to spam/ email_id| : ', 'yellow')).capitalize()
+        colored("|Who do you want to spam/ email_id| : ", "yellow")
+    ).capitalize()
 
-    if not '@gmail.com' in whom:
+    if not "@gmail.com" in whom:
         where = input(
-            colored('|Where do you want to spam| : ', 'yellow')).capitalize()
+            colored("|Where do you want to spam| : ", "yellow")).capitalize()
 
         while True:
 
             try:
-                count = int(
-                    input(colored('|How many times|: ', 'yellow'))
-                )
+                count = int(input(colored("|How many times|: ", "yellow")))
 
-                if 'Telegram' in where:
-                    print(colored('\nSpamming in Telegram...\n', 'blue'))
-                    open_app('Telegram')
+                if "Telegram" in where:
+                    print(colored("\nSpamming in Telegram...\n", "blue"))
+                    open_app("Telegram")
                     search(whom)
                     spamit(spam)
 
-                elif 'Whatsapp' in where:
-                    print(colored('\nSpamming in Whatsapp...\n', 'blue'))
-                    open_app('Whatsapp')
+                elif "Whatsapp" in where:
+                    print(colored("\nSpamming in Whatsapp...\n", "blue"))
+                    open_app("Whatsapp")
                     time.sleep(5)
                     search(whom)
                     time.sleep(4)
                     spamit(spam)
 
-                elif 'Others' in where:
-                    print(colored(
-                        'Put your cursor where you want to spam,\nSpamming will start in 10 seconds...', 'blue'))
+                elif "Others" in where:
+                    print(
+                        colored(
+                            "Put your cursor where you want to spam,\nSpamming will start in 10 seconds...",
+                            "blue",
+                        )
+                    )
                     time.sleep(10)
                     spamit(spam)
 
@@ -86,34 +97,36 @@ if __name__ == '__main__':
 
                 for _ in range(count - 1):
                     pyautogui.write(spam)
-                    pyautogui.press('Enter')
+                    pyautogui.press("Enter")
 
-                print(colored('\nSpam sent!\n', 'cyan', attrs=['bold']))
+                print(colored("\nSpam sent!\n", "cyan", attrs=["bold"]))
                 break
 
             except ValueError:
                 print(
-                    colored('\nNote: Enter a number, please!\n',
-                            'red', attrs=['underline'])
+                    colored(
+                        "\nNote: Enter a number, please!\n", "red", attrs=["underline"]
+                    )
                 )
                 continue
 
-    elif '@gmail.com' in whom:
-        counts = int(input(colored('|How many times|: ', 'yellow')))
-        print(colored('\nSpamming through Email...\n', 'blue'))
+    elif "@gmail.com" in whom:
+        counts = int(input(colored("|How many times|: ", "yellow")))
+        print(colored("\nSpamming through Email...\n", "blue"))
         context = ssl.create_default_context()
 
         try:
-            server = smtplib.SMTP('smtp.gmail.com', 587)
+            credentials = get_credentials()
+            server = smtplib.SMTP("smtp.gmail.com", 587)
             server.ehlo()
             server.starttls(context=context)
             server.ehlo()
-            server.login('your_email id',
-                         'your_server')
+            email = credentials["email"]
+            password = credentials["password"]
+            server.login(email, password)
             for i in range(counts):
-                server.sendmail('your_email id',
-                                whom, spam)
-            print(colored('\nSpam sent!\n', 'cyan', attrs=['bold']))
+                server.sendmail(email, whom, spam)
+            print(colored("\nSpam sent!\n", "cyan", attrs=["bold"]))
             exit()
 
         except Exception as e:
